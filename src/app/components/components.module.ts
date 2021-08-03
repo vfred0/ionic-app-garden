31114e6af700f445c3fe330a717d5f112b86aafa
Victor Arreaga <Victor Arreaga>
2021-08-03 17:09:41 -0500

    refactor

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from "@angular/common";
 import { IonicModule } from "@ionic/angular";
 import { RecomendationsComponent } from "./recomendations/recomendations.component";
 import { StagesComponent } from "./stages/stages.component";
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { HeroBackMenuComponent } from "./hero-back-menu/hero-back-menu.component";
 import { AboutPlantComponent } from "./about-plant/about-plant.component";
 import { PlantPageRoutingModule } from "../pages/plant/plant-routing.module";
@@ -11,11 +11,11 @@ import { SlidesComponent } from "./slides/slides.component";
 import { LogoComponent } from "./logo/logo.component";
 import { OptionComponent } from "./option/option.component";
 import { MenuOptionsComponent } from "./menu-options/menu-options.component";
-import { AppRoutingModule } from "../app-routing.module";
 import { ButtonComponent } from "./button/button.component";
 import { TemperaturesComponent } from "./temperatures/temperatures.component";
 import { BoxTemperatureComponent } from "./box-temperature/box-temperature.component";
 import { SelectComponent } from "./select/select.component";
+import { MeasurementsComponent } from "./measurements/measurements.component";
 
 
 @NgModule({
@@ -31,7 +31,8 @@ import { SelectComponent } from "./select/select.component";
     ButtonComponent,
     TemperaturesComponent,
     BoxTemperatureComponent,
-    SelectComponent
+    SelectComponent,
+    MeasurementsComponent
   ],
   exports: [
     StagesComponent,
@@ -44,13 +45,14 @@ import { SelectComponent } from "./select/select.component";
     ButtonComponent,
     TemperaturesComponent,
     BoxTemperatureComponent,
-    SelectComponent
-
+    SelectComponent,
+    MeasurementsComponent
   ],
   imports: [
     CommonModule,
     IonicModule,
-    FormsModule,
+    // FormsModule,
+    ReactiveFormsModule,
     PlantPageRoutingModule
   ]
 })

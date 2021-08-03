c04908696d394cfd2b202911d25f1311b2e29fff
Victor Arreaga <Victor Arreaga>
2021-08-03 17:04:00 -0500

    refactor

--- src/app/pages/land-calculator/land-calculator.page.ts
+++ src/app/pages/land-calculator/land-calculator.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { BackMenu, Button } from 'src/app/interfaces/interfaces';
+import { BackMenu, Button, ControlName } from 'src/app/interfaces/interfaces';
+import { GardenStyleService } from 'src/app/services/gardenStyle/garden-style.service';
 
 @Component({
   selector: 'app-land-calculator',
@@ -11,21 +12,16 @@ export class LandCalculatorPage implements OnInit {
   private optionsBackMenu: BackMenu;
   private buttons: Button[];
 
-  constructor() {
-    this.buttons = [
-      {
-        title: "Maceta",
-        class: "o-button-color-g100"
-      },
-      {
-        title: "Lecho elevado",
-        class: "button-disable o-button-color-w200"
-      }
-    ];
+  constructor(private gardenStyle: GardenStyleService) {
+    this.buttons = new Array();
     this.optionsBackMenu = {
       image: "/assets/images/land-calculator.png",
       link: "/home"
     }
+  }
+
+  getRecomendations(): String[] {
+    return this.gardenStyle.getRecomendations();
   }
 
   getOptionsBackMenu(): BackMenu {
@@ -36,8 +32,19 @@ export class LandCalculatorPage implements OnInit {
     return this.buttons;
   }
 
-  eventButton(button: Button) {
-    console.log("presionado boton estilo ", button)
+  setResults(controlName: ControlName[]) {
+    this.gardenStyle.setResults(controlName);
+  }
+
+  getShapeOptions(): String[] {
+    return this.gardenStyle.getShapeOptions();
+  }
+
+  onChangeShapeOption(selectOption: String) {
+    this.gardenStyle.setShapeOption(selectOption);
+  }
+
+  onChangeSelectButton(button: Button) {
     if (button.class.includes('disable')) {
       this.buttons.forEach(b => {
         if (b.title != button.title) {
@@ -46,8 +53,28 @@ export class LandCalculatorPage implements OnInit {
       })
     }
     button.class = 'button-enable o-button-color-g100';
+    this.gardenStyle.setNameStyle(button.title);
   }
+
+  getMeasurements(): ControlName[] {
+    return this.gardenStyle.getControlName();
+  }
+
   ngOnInit() {
+    this.gardenStyle.getAllNameGardenStyles().forEach((nameStyle, index) => {
+      if (index == 0) {
+        this.buttons.push({
+          title: nameStyle,
+          class: "o-button-color-g100"
+        })
+      } else {
+        this.buttons.push({
+          title: nameStyle,
+          class: "button-disable o-button-color-w200"
+        })
+      }
+    });
+
   }
 
 }
